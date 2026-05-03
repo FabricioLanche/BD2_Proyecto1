9a34f19b253f4b17faa47f1182815b43e4ee8a6b
Luciana Yangali <luciana.yangali@utec.edu.pe>
2026-05-03 13:56:37 -0500

    feat:serialization methods for basic structures

--- Backend/DBMS/indexes/b+tree.py
+++ Backend/DBMS/indexes/b+tree.py
@@ -1,6 +1,7 @@
 import struct
 from dataclasses import dataclass, field
-from typing import List, Tuple
+from typing import List, Tuple, Optional
+#from organization.page_manager import PageManager
 
 @dataclass
 class BPlusHeader:
@@ -39,11 +40,40 @@ class BPlusNode:
     header: NodeHeader
     keys: List[int] = field(default_factory=list)
 
+    def serialize(self, page_size: int) -> bytes:
+        data = bytearray(page_size)
+        data[:NodeHeader.SIZE] = self.header.to_bytes()
+        offset = NodeHeader.SIZE
+        
+        # Serializar claves
+        for k in self.keys:
+            struct.pack_into('<i', data, offset, k)
+            offset += 4
+        return data, offset
+
 @dataclass
 class LeafNode(BPlusNode):
-    values: List[Tuple[int, int]] = field(default_factory=list)  # Representa (PageID, SlotID)
+    values: List[Tuple[int, int]] = field(default_factory=list)
     next_leaf: int = -1
+
+    def to_bytes(self, page_size: int) -> bytes:
+        data, offset = super().serialize(page_size)
+        # Serializar Values (RID: PageID, SlotID)
+        for p, s in self.values:
+            struct.pack_into('<ii', data, offset, p, s)
+            offset += 8
+        # Serializar puntero a siguiente hoja
+        struct.pack_into('<i', data, offset, self.next_leaf)
+        return bytes(data)
 
 @dataclass
 class InternalNode(BPlusNode):
     children: List[int] = field(default_factory=list)
+
+    def to_bytes(self, page_size: int) -> bytes:
+        data, offset = super().serialize(page_size)
+        # Serializar hijos (siempre hay num_keys + 1)
+        for c in self.children:
+            struct.pack_into('<i', data, offset, c)
+            offset += 4
+        return bytes(data)
